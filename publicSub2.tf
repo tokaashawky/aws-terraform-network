e109d0a43dabf1ce87f3a12c7fd1b0cbc7788aea
Toka Shawky Elmaadawy <119989493+tokaashawky@users.noreply.github.com>
2025-05-21 01:53:36 +0300

    publicSub2.tf
    instances launched into the subnet should be assigned a public IP address

--- publicSub2.tf
+++ publicSub2.tf
@@ -2,6 +2,7 @@ resource "aws_subnet" "PublicSubnet2" {
   vpc_id     = aws_vpc.MyVPC.id
   cidr_block = "10.10.2.0/24"
   availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
   tags = {
     Name = "PublicSubnet2"
   }
